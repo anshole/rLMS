64fc67c6313933d9ef2c5dbe9695daf4478b03a1
A. Vyas <anshzinc@gmail.com>
2015-01-31 19:28:55 +0530

    Add dev config?

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,21 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.mailer = "Devise::Mailer"
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => '587',
+  :domain               => 'gmail.com',
+  :user_name            => 'email_here',
+  :password             => 'password_here',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
+
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
